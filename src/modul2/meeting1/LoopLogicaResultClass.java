6fbbce8373a27a436481aa13dd080b86cdbb131a
bes1ks0n <godovanyk@hotmail.com>
2016-08-20 23:53:50 +0100

    20.08.2016 meeting 1 MODULE 1

--- src/modul2/meeting1/LoopLogicaResultClass.java
+++ src/modul2/meeting1/LoopLogicaResultClass.java
@@ -2,23 +2,43 @@ package modul2.meeting1;
 
     public class LoopLogicaResultClass {
         public static void main(String[] args) {
-
-
 /* Loop(кольцо) - цыкл
             Есть два цыкла в java
             "for" и "while"
-            for состоит из  3х частей это (-variable(переменная)- -condition(условие)- -что делать после окончания цыкла-
-*/
-        for (int p = 43; p < 100; p++) {
-            System.out.println("people +" + p);
+            for состоит из  3х частей это
+            (-variable(переменная)- -condition(условие)- -что делать после окончания цыкла-
+                            |                      |                             |
+        for             (int p = 43;            p < 100;                         p++) {
+                                                                                 |
+                                                                        System.out.println("people +" + p);     */
+            boolean isActive = true;
+            for (int p = 43; p < 100; p++) {
+                System.out.println(+ p);
+                // brack continue - нельзя совмещать вместе ! Не работають вместе !
+                // brack;  - Принудительно выйти их цыкла
+                if (isActive) {  // еслм в начале поставить  -> "!" <-   выражение автоматички переходить в c true в false или на оборот
+                    //break;
+                    continue;// продолжай выполнять
+                }
+                System.out.println("after continue"); //если условия выполняеться в continue то println непоявляеться.
+            }
 
-            // brack continue - нельзя совмещать вместе ! Не работають вместе !
-            // brack;  - Принудительно выйти их цыкла
-            //
+                int w = 100;
+                while (w > 42) {  // while принемает только одно условие  в кавычках
+                    System.out.println(w);
+                    w--;
+
+                 /*   int n = -10;      // Операция do в любом случаии выполняеться один  ра в любом true или false
+                    do {
+                        System.out.println("Count is:" + n);
+                        n++;
+                    }
+                 */
+                }
+            }
         }
 
 
-        }
-    }
+
 
 
