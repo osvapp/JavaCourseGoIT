3623b9bca16e57fb14a62e71e107065ae707d980
bes1ks0n <godovanyk@hotmail.com>
2016-08-23 23:43:26 +0100

    20-21.08.2016 meeting 1 MODULE 1
    homework2 - start

---
+++ src/modul2/homework1/Array.java
@@ -0,0 +1,47 @@
+package modul2.homework1;
+
+public class Array {
+
+    static int sun(int[] array) {
+        int sum = 0;
+        for (int i = 0; i < array.length; i++) {
+            sum += array[i];
+        }
+        return sum;
+    }
+
+    static double sun(double[] array) {
+        double sum = 0;
+        for (int i = 0; i < array.length; i++) {
+            sum += array[i];
+        }
+        return sum;
+    }
+
+
+    public static int min(int[] array) {
+        int min = 0;
+        for (int i = 0; i > array.length; i++) {
+            min = array[i];
+        }
+        return 0;
+    }
+public static double min(double[]array){
+    double = 0;
+    for (double i = 0;i > array.length; i--){
+       double min = array[i];
+    }
+return 0;
+}
+
+    public static void main(String[] args) {
+           int[] nm = {1, 4, 125, -75, 35, 235, -55, 8985, -8255, -850};
+           double[] nm1 = {1.2, 43.4, 654.8, -24.235, 43.674, 4.78, -41.3, 86.23, 95.2886, 10.445};
+
+
+
+          System.out.println(sun(nm));
+          System.out.println(min(nm));
+        //  System.out.println(sun(nm1));
+    }
+}
