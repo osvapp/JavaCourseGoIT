b1deb2774e9bef91644c5851a95aee5a651cc010
bes1ks0n <godovanyk@hotmail.com>
2016-08-24 21:51:16 +0100

    20-21.08.2016 meeting 1 MODULE 1
    ZERO!!!!

---
+++ src/modul2/homework1/min.java
@@ -0,0 +1,18 @@
+package modul2.homework1;
+
+public class min {
+    public static void main(String[] args) {
+
+        int [] array = {31, 32, 4, 5, 66, 77, 555, 44, 33, 212};
+        int max = array[0];
+        // for (int arrrrr : Array) { }   не  получилось
+        for (int m = 0; m < array.length; m++){
+            if (array[m] > max) max = array[m];
+        }
+         System.out.println(max);
+
+
+
+
+    }
+}
