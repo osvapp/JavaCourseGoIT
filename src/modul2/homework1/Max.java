7b15c3fee346bed18fde70dbb6f7b14516618f6c
bes1ks0n <godovanyk@hotmail.com>
2016-08-26 09:14:44 +0100

    16n times trying to do homework ;(

--- src/modul2/homework1/Max.java
+++ src/modul2/homework1/Max.java
@@ -5,7 +5,6 @@ public class Max {
 
         int[] array = {31, 32, 4, 5, 66, 77, 555, 44, 33, 212};
         int max = array[0];
-        // for (int arrrrr : Array) { }   не  получилось
         for (int m = 0; m < array.length; m++) {
             if (array[m] > max) max = array[m];
         }
